d54806fbc23653727f70ecc5935c2862466f3e30
Satendra Kumar <satendrakumar06@gmail.com>
2021-03-09 14:08:28 +0530

    State managment example

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 
 import Welcome from "./examples/WelcomeC";
 //import Welcome from "./examples/WelcomeF";
-
+import  Clock from  "./examples/state/Clock"
 
 
 function App() {
   return (
     <div className="App">
         <Welcome name="Bob" />
+        <Clock />
     </div>
   );
 }
---
+++ src/examples/state/Clock.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+/**
+ * State management example
+ */
+class Clock extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.TimerID)
+
+    }
+
+    tick() {
+        this.setState({date: new Date()})
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+
+}
+
+export default Clock;
